e8de2552ea34b0d69403bc06ee3724bd7173c804
Filip Majewski <majerihno@MacBook-Pro-Filip.local>
2023-02-23 16:10:20 +0100

    Added project

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect, useState } from "react";
+
+import CartIcon from "../Cart/CartIcon";
+import style from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
+
+const HeaderCartButton = props => {
+
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+    const {items} = cartCtx;
+
+    const btnStyle = `${style.button} ${btnIsHighlighted ? style.bump : ''}`;
+
+    useEffect(() => {
+        if(items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
+    return (
+        <button className={btnStyle} onClick={props.onClick} >
+            <span className={style.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className={style.badge}>{numberOfCartItems}</span>
+        </button>
+    );
+};
+
+export default HeaderCartButton;
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import style from "./Modal.module.css";
+
+const Backdrop = props => {
+    return(
+        <div className={style.backdrop} onClick={props.onClose} />
+    );
+};
+
+const ModalOverlay = props => {
+    return(
+        <div className={style.modal}>
+            {props.children}
+        </div>
+    );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = props => {
+  return (
+      <React.Fragment>
+          {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)};
+          {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)};
+      </React.Fragment>
+  );
+};
+
+export default Modal;
